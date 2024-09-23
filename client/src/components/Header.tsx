7a6a6db5083a769da969dce20f1e84ca87ee94f9
TzivyaFireman <tz0534199716@gmail.com>
2024-09-24 01:24:32 +0300

    add Header and ContactPage components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Typography, Button, AppBar, Toolbar } from '@mui/material';
+
+const Header: React.FC = () => {
+    return (
+    <AppBar position="static" sx={{ backgroundColor: '#121212' }}>
+        <Toolbar>
+            <Box sx={{ display: 'flex', flexGrow: 1 }}>
+                <Button color="inherit" href="#contact">יצירת קשר</Button>
+                <Button color="inherit" href="#appointments">לקביעת תורים</Button>
+                <Button color="inherit" href="#articles">מאמרים</Button>
+                <Button color="inherit" href="#news">חדשות האתר</Button>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant="h6" component="div">
+                    שלווה גמורה
+                </Typography>
+                <img
+                    src="/images/shalvagmuralogo.png"
+                    style={{ width: '40px', height: '40px', marginRight: '10px' }}
+                />
+
+            </Box>
+        </Toolbar>
+    </AppBar>
+    )
+}
+export default Header;
