5eee692b6e8bba55ddf66e4bfbf440fe12046f39
TzivyaFireman <tz0534199716@gmail.com>
2024-09-25 14:43:26 +0300

    Enable cors

--- server/app.ts
+++ server/app.ts
@@ -4,9 +4,11 @@ import userRoutes from './routers/userRoutes';
 import appointmentRoutes from './routers/appointmentRoutes';
 import serviceRoutes from './routers/serviceRoutes';
 import articleRoutes from './routers/articleRouter';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/', articleRoutes);
 app.use('/', userRoutes);
 app.use('/', appointmentRoutes);
